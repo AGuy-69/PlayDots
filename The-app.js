545a8f894731bd8febf2762ab1d0ca49d216ac88
AGuy-69 <ilikepython123@gmail.com>
2025-05-29 14:20:37 +0700

    Update The-app.js

--- The-app.js
+++ The-app.js
@@ -278,7 +278,9 @@
     document.getElementById('finalTotalDisplay').innerText = 'Rp ' + final.toLocaleString('id-ID', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
     
     const billingBox = document.querySelector('.Billing');
+    const carttext = document.querySelector('.cart-text');
     billingBox.style.display = sub > 0 ? 'flex' : 'none';
+    carttext.style.display = sub > 0 ? 'flex' : 'none';
   }
 
   function addToCart(productId) {
